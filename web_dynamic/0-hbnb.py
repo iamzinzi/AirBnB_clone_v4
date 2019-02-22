2bc5efe21d2eff9db6527832fc55676c3e11c4cb
Jinji Zhang <362@holbertonschool.com>
2019-02-22 21:55:58 +0000

    Fix: typecast uuid to str type for jinja templating

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -35,7 +35,7 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    cache_id = uuid4()
+    cache_id = str(uuid4())
     return render_template('0-hbnb.html',
                            states=states,
                            amens=amens,
