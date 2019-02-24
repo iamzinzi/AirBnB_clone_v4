311cd24ba39790889f7fa677d28dead5810e1fc1
Jinji Zhang <362@holbertonschool.com>
2019-02-24 00:58:26 +0000

    semistandard fix

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -6,15 +6,15 @@ $(function () {
     } else {
       delete checkedItems[$(this).data('id')];
     }
-    let filler_text = Object.values(checkedItems).join(', ');
-    $('div.amenities h4').text(filler_text);
+    let fillerText = Object.values(checkedItems).join(', ');
+    $('div.amenities h4').text(fillerText);
   });
 });
 
 $(function () {
   const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(url, function (data, status) {
-    if (status == "success") {
+    if (status === 'success') {
       $('div#api_status').addClass('available');
     } else {
       $('div#api_status').removeClass('available');
@@ -33,38 +33,38 @@ $(function () {
     dataType: 'json',
     success: function (data) {
       for (let place of data) {
-	let articleText = '<article>' +
-	  '<div class="title">' +
-	  '<h2>' + place.name + '</h2>' +
-	  '<div class="price_by_night">' +
-	  '$' + place.price_by_night + '</div>' +
-	  '</div>' +
-	  '<div class="information">' +
-	  '<div class="max_guest">' +
-	  '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
-	  '<br />' +
-	  place.max_guest + ' Guests' +
-	  '</div>' +
-	  '<div class="number_rooms">' +
-	  '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
-	  '<br />' +
-	  place.number_rooms + ' Bedrooms' +
-	  '</div>' +
-	  '<div class="number_bathrooms">' +
-	  '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
-	  '<br />' +
-	  place.number_bathrooms + ' Bathroom' +
-	  '</div>' +
-	  '</div>' +
-	  '</article>'
-	$('section.places').append(articleText);
+        let articleText = '<article>' +
+          '<div class="title">' +
+          '<h2>' + place.name + '</h2>' +
+          '<div class="price_by_night">' +
+          '$' + place.price_by_night + '</div>' +
+          '</div>' +
+          '<div class="information">' +
+          '<div class="max_guest">' +
+          '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
+          '<br />' +
+          place.max_guest + ' Guests' +
+          '</div>' +
+          '<div class="number_rooms">' +
+          '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
+          '<br />' +
+          place.number_rooms + ' Bedrooms' +
+          '</div>' +
+          '<div class="number_bathrooms">' +
+          '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
+          '<br />' +
+          place.number_bathrooms + ' Bathroom' +
+          '</div>' +
+          '</div>' +
+          '</article>';
+        $('section.places').append(articleText);
       }
     }
   });
 });
 
 $(function () {
-  const amenities = {'amenities': []};
+  const amenities = { 'amenities': [] };
   $('input:checkbox').change(function () {
     if ($(this).is(':checked')) {
       amenities.amenities.push($(this).data('id'));
@@ -83,32 +83,35 @@ $(function () {
       },
       dataType: 'json',
       success: function (data) {
+        $('article').remove();
+        console.log('data is this long');
+        console.log(data.length);
         for (let place of data) {
-	  let articleText = '<article>' +
-	      '<div class="title">' +
-	      '<h2>' + place.name + '</h2>' +
-	      '<div class="price_by_night">' +
-	      '$' + place.price_by_night + '</div>' +
-	      '</div>' +
-	      '<div class="information">' +
-	      '<div class="max_guest">' +
-	      '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
-	      '<br />' +
-	      place.max_guest + ' Guests' +
-	      '</div>' +
-	      '<div class="number_rooms">' +
-	        '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
-	        '<br />' +
-	        place.number_rooms + ' Bedrooms' +
-	        '</div>' +
-	        '<div class="number_bathrooms">' +
-	        '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
-	        '<br />' +
-	        place.number_bathrooms + ' Bathroom' +
-	        '</div>' +
-	        '</div>' +
-	        '</article>'
-	  $('section.places').append(articleText);
+          let articleText = '<article>' +
+              '<div class="title">' +
+              '<h2>' + place.name + '</h2>' +
+              '<div class="price_by_night">' +
+              '$' + place.price_by_night + '</div>' +
+              '</div>' +
+              '<div class="information">' +
+              '<div class="max_guest">' +
+              '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
+              '<br />' +
+              place.max_guest + ' Guests' +
+              '</div>' +
+              '<div class="number_rooms">' +
+                '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
+                '<br />' +
+                place.number_rooms + ' Bedrooms' +
+                '</div>' +
+                '<div class="number_bathrooms">' +
+                '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
+                '<br />' +
+                place.number_bathrooms + ' Bathroom' +
+                '</div>' +
+                '</div>' +
+                '</article>';
+          $('section.places').append(articleText);
         }
       }
     });
